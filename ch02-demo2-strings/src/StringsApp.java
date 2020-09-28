6c05d237ac124e8801876f84e44e6067380aeeaf
Sean Blessing <snblessing@gmail.com>
2020-09-28 16:35:35 -0400

    Scanner nextLine() demo

--- ch02-demo2-strings/src/StringsApp.java
+++ ch02-demo2-strings/src/StringsApp.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
 
 public class StringsApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// get input from user via Scanner
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// get an int
+		System.out.println("Enter a whole#: ");
+		int n1 = sc.nextInt();
+		
+		// get a word
+		System.out.print("Enter a word: ");
+		String word = sc.next();
+		// get a line of text
+		System.out.println("Enter a line of text:");
+		sc.nextLine();
+		String sentence = sc.nextLine();
+		
+		System.out.println("Your sentence: "+sentence);
+		
+		System.out.println("Bye!");
 
 	}
 
