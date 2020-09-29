a2ad3af480aee52b4b710684c5ec78887cfd3083
Sean Blessing <snblessing@gmail.com>
2020-09-29 14:35:15 -0400

    ch5 instruction

--- ch05-demo1-methods/src/BasicCalculatorApp.java
+++ ch05-demo1-methods/src/BasicCalculatorApp.java
@@ -14,10 +14,20 @@ public class BasicCalculatorApp {
 		int a = getInt("Enter a whole number: ", sc);
 		int b = getInt("Enter another whole number: ", sc);
 		System.out.println("Sum of "+a+"+"+b+" = " + sum(a, b));
+		
+		double c = getDouble("Enter a decimal number: ", sc);
+		double d = getDouble("Enter another decimal number: ", sc);
+		System.out.println("Sum of "+c+"+"+d+" = " + sum(c, d));
 
+		// use getIntWithinRange to validate a month entry (1-12)
+		int month = getIntWithinRange("Enter a month number: ",sc,1,12);
+		System.out.println("month entered = "+month);
+		
 		sc.close();
 	}
 	
+	// get an integer from console
+	// use exception handling to check input
 	private static int getInt(String prompt, Scanner sc) {
 		int nbr = 0;
 		while (true) {
@@ -26,14 +36,53 @@ public class BasicCalculatorApp {
 				nbr = sc.nextInt();
 				break;
 			} catch (InputMismatchException ime) {
-				System.out.println("Error!  Invalid number.  Try again.\n");
+				System.out.println("Error!  Invalid integer.  Try again.\n");
 				sc.nextLine();
 				continue;
 			}
 		}
 		return nbr;
 	}
+	
+	private static int getIntWithinRange(String prompt, Scanner sc, int min, int max) {
+		int nbr = 0;
+		boolean isValid = false;
+		
+		while (!isValid) {
+			nbr = getInt(prompt, sc);
+			if (nbr < min) {
+				System.out.println("Error!  Number must be >= "+min+".");
+			}
+			else if (nbr > max) {
+				System.out.println("Error!  Number must be <= "+max+".");
+			}
+			else {
+				isValid = true;
+			}
+		}
+		
+		return nbr;
+		
+	}
 
+	// get a double from console
+	// use data validation to check input
+	private static double getDouble(String prompt, Scanner sc) {
+		double nbr = 0;
+		while (true) {
+			System.out.print(prompt);
+			if (sc.hasNextDouble()) {
+				nbr = sc.nextDouble();				
+				break;
+			}
+			else {
+				System.out.println("Error!  Invalid double.  Try again.\n");
+				sc.nextLine();
+				continue;
+			}
+		}
+		return nbr;
+	}
 	// method, no inputs and no return type
 	private static void printWelcomeMessage() {
 		System.out.println("Welcome to the basic calculator app!");
@@ -55,4 +104,9 @@ public class BasicCalculatorApp {
 		return sum;
 	}
 
+	// 2 inputs and a return type
+	// accept 2 doubles, return the sum
+	private static double sum(double a, double b) {
+		return a+b;
+	}
 }
