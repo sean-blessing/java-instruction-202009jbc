dd196632714648252d6cdb2d950d52cc44a6bf65
Sean Blessing <snblessing@gmail.com>
2020-10-09 13:40:35 -0400

    file processing

--- product-manager/src/db/ProductTextFile.java
+++ product-manager/src/db/ProductTextFile.java
@@ -1,10 +1,23 @@
 package db;
 
+import java.io.*;
+import java.nio.file.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import business.Product;
 
 public class ProductTextFile implements DAO<Product> {
+	private List<Product> products = null;
+	private Path productsPath = null;
+	private File productsFile = null;
+	private final String FIELD_SEP = "\t";
+
+	public ProductTextFile() {
+		productsPath = Paths.get("products.txt");
+		productsFile = productsPath.toFile();
+		products = getAll();
+	}
 
 	@Override
 	public Product get(String code) {
@@ -12,15 +25,34 @@ public class ProductTextFile implements DAO<Product> {
 		return null;
 	}
 
+	// get all products from text file if our list is null
 	@Override
 	public List<Product> getAll() {
-		// TODO Auto-generated method stub
+		if (products != null) {
+			return products;
+		}
+		products = new ArrayList<>();
+		if (Files.exists(productsPath)) {
+			try (BufferedReader in = new BufferedReader(
+									 new FileReader(productsFile))) {
+				
+			}
+			catch (IOException ioe) {
+				System.out.println(ioe);
+				return null;
+			}
+			
+		}
+		else {
+			System.out.println(productsPath.toAbsolutePath()+ " doesn't exist.");
+			return null;
+		}
+		
 		return null;
 	}
 
 	@Override
 	public boolean add(Product t) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
