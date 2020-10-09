a38310d1a6045d72e815e7bb38de12f359183ba8
Sean Blessing <snblessing@gmail.com>
2020-10-09 14:26:34 -0400

    Product Manager text file project

--- product-manager/src/db/ProductTextFile.java
+++ product-manager/src/db/ProductTextFile.java
@@ -33,39 +33,72 @@ public class ProductTextFile implements DAO<Product> {
 		}
 		products = new ArrayList<>();
 		if (Files.exists(productsPath)) {
-			try (BufferedReader in = new BufferedReader(
-									 new FileReader(productsFile))) {
-				
-			}
-			catch (IOException ioe) {
+			try (BufferedReader in = new BufferedReader(new FileReader(productsFile))) {
+				// read products from file into our list
+				String line = in.readLine();
+				while (line != null) {
+					String[] fields = line.split(FIELD_SEP);
+					String code = fields[0];
+					String description = fields[1];
+					String price = fields[2];
+					double priceDbl = Double.parseDouble(price);
+					Product p = new Product(code, description, priceDbl);
+					products.add(p);
+					line = in.readLine();
+				}
+			} catch (IOException ioe) {
 				System.out.println(ioe);
 				return null;
 			}
-			
-		}
-		else {
-			System.out.println(productsPath.toAbsolutePath()+ " doesn't exist.");
+
+		} else {
+			System.out.println(productsPath.toAbsolutePath() + " doesn't exist.");
 			return null;
 		}
-		
-		return null;
+
+		return products;
 	}
 
 	@Override
-	public boolean add(Product t) {
-		return false;
+	public boolean add(Product p) {
+		products.add(p);
+		return saveAll();
 	}
 
 	@Override
-	public boolean update(Product t) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean update(Product p) {
+		// get old product and remove it
+		Product oldProduct = this.get(p.getCode());
+		int i = products.indexOf(oldProduct);
+		products.remove(i);
+		products.add(i, p);
+		return saveAll();
 	}
 
 	@Override
-	public boolean delete(Product t) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean delete(Product p) {
+		products.remove(p);
+		return saveAll();
+	}
+	
+	private boolean saveAll() {
+		// after every maintenance (add, update, delete) function,
+		// rewrite the text file
+		try (PrintWriter out = new PrintWriter(
+				  			   new BufferedWriter(
+				  			   new FileWriter(productsFile)))) {
+			// write all products in the list to the file
+			for (Product p: products) {
+				out.print(p.getCode()+FIELD_SEP);
+				out.print(p.getDescription()+FIELD_SEP);
+				out.println(p.getPrice());
+			}
+			return true;
+		}
+		catch (IOException ioe) {
+			System.out.println(ioe);
+			return false;
+		}
 	}
 
 }
