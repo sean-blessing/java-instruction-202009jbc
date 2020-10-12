ddb1b4784d914f100ebe8dfec05abf39b3ff782a
Sean Blessing <snblessing@gmail.com>
2020-10-12 11:51:36 -0400

    Added better exception handling

--- ch15-demo1-file-processing/src/ui/FileProcessingApp.java
+++ ch15-demo1-file-processing/src/ui/FileProcessingApp.java
@@ -5,14 +5,19 @@ import java.nio.file.*;
 
 public class FileProcessingApp {
 
-	public static void main(String[] args) throws IOException  {
+	public static void main(String[] args)  {
 		System.out.println("Processing files!");
 		//p. 463
 		// create a 'temp' directory on our hard drive
 		String dirString = "c:/temp/sub1/sub2";
 		Path dirPath = Paths.get(dirString);
 		if (Files.notExists(dirPath)) {
-			Files.createDirectories(dirPath);
+			try {
+				Files.createDirectories(dirPath);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			System.out.println("directory created: "+dirPath.getFileName());
 		}
 		
@@ -20,7 +25,12 @@ public class FileProcessingApp {
 		String fileString = "products.txt";
 		Path filePath = Paths.get(dirString, fileString);
 		if (Files.notExists(filePath)) {
-			Files.createFile(filePath);
+			try {
+				Files.createFile(filePath);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 		// display file info
@@ -31,15 +41,20 @@ public class FileProcessingApp {
 		if (Files.exists(dirPath) && Files.isDirectory(dirPath)) {
 			System.out.println("Directory: "+dirPath.toAbsolutePath());
 			System.out.println("Files:");
-			DirectoryStream<Path> dirStream = Files.newDirectoryStream(dirPath);
-			for (Path p: dirStream) {
-				if (Files.isRegularFile(p)) {
-					System.out.println("     "+p.getFileName());
-				}
-				// Sean to figure this out
+			DirectoryStream<Path> dirStream;
+			try {
+				dirStream = Files.newDirectoryStream(dirPath);
+				for (Path p: dirStream) {
+					if (Files.isRegularFile(p)) {
+						System.out.println("     "+p.getFileName());
+					}
+					// Sean to figure this out
 //				else if (Files.isDirectory(dirPath)) {
 //					System.out.println(p.getParent().toString());
 //				}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 		
@@ -48,21 +63,36 @@ public class FileProcessingApp {
 		Path productsPath = Paths.get("products.txt");
 		File productsFile = productsPath.toFile();
 		
-		PrintWriter out = new PrintWriter(
-						  new BufferedWriter(
-						  new FileWriter(productsFile)));
-		// write data to the stream
-		out.println("java\tMurach's Java Programming\t57.50");
-		
-		out.close();
+		PrintWriter out;
+		try {
+			out = new PrintWriter(
+							  new BufferedWriter(
+							  new FileWriter(productsFile)));
+			// write data to the stream
+			out.println("java\tMurach's Java Programming\t57.50");
+			
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 		// read data from the file
-		BufferedReader in = new BufferedReader(
-							new FileReader(productsFile));
-		String line = in.readLine();
-		System.out.println(line);
-		in.close();
+		BufferedReader in;
+		try {
+			in = new BufferedReader(
+								new FileReader(productsFile));
+			String line = in.readLine();
+			System.out.println(line);
+			in.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 		
