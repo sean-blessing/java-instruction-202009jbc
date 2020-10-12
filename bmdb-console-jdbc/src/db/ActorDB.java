e16d7e10885523492fdad072ad1f6d0d52eb72b1
Sean Blessing <snblessing@gmail.com>
2020-10-12 16:20:08 -0400

    added ActorDB

--- bmdb-console-jdbc/src/db/ActorDB.java
+++ bmdb-console-jdbc/src/db/ActorDB.java
@@ -23,30 +23,34 @@ public class ActorDB implements DAO<Actor> {
 
 	@Override
 	public Actor get(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Actor a = null;
+		String sql = "select * from actor where id = ?";
+		try (Connection conn = getConnection();
+			 PreparedStatement ps = conn.prepareStatement(sql)) {
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			// we only expect 1 row
+			if (rs.next()) {
+				a = getActorFromResultSet(rs);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return a;
 	}
 
 	@Override
 	public List<Actor> getAll() {
 		List<Actor> actors = new ArrayList<>();
 		//p. 679
-		Connection conn = null;
-		try {
-			conn = getConnection();
-			String sql = "select * from actor;";
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
-			
+		
+		String sql = "select * from actor;";
+		try (Connection conn = getConnection();
+			 PreparedStatement ps = conn.prepareStatement(sql);
+			 ResultSet rs = ps.executeQuery(sql);) {
 			//p. 681 - process result set
 			while (rs.next()) {
-				int id = rs.getInt(1);
-				String fn = rs.getString(2);
-				String ln = rs.getString(3);
-				String g = rs.getString(4);
-				String bdStr = rs.getString(5);
-				LocalDate bd = LocalDate.parse(bdStr);
-				Actor a = new Actor(id,fn,ln,g,bd);
+				Actor a = getActorFromResultSet(rs);
 				actors.add(a);
 			}
 			
@@ -55,33 +59,81 @@ public class ActorDB implements DAO<Actor> {
 			e.printStackTrace();
 			actors = null;
 		}
-		finally {
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
 		return actors;
 	}
 
-	@Override
-	public boolean add(Actor t) {
-		// TODO Auto-generated method stub
-		return false;
+	private Actor getActorFromResultSet(ResultSet rs) throws SQLException {
+		int id = rs.getInt(1);
+		String fn = rs.getString(2);
+		String ln = rs.getString(3);
+		String g = rs.getString(4);
+		String bdStr = rs.getString(5);
+		LocalDate bd = LocalDate.parse(bdStr);
+		Actor a = new Actor(id,fn,ln,g,bd);
+		return a;
 	}
 
 	@Override
-	public boolean update(Actor t) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean add(Actor a) {
+		boolean success = false;
+		String sql = "insert into actor values "
+				+ "(?,?,?,?,?)";
+		try (Connection conn = getConnection();
+			 PreparedStatement ps = conn.prepareStatement(sql)) {
+			ps.setInt(1, a.getId());
+			ps.setString(2, a.getFirstName());
+			ps.setString(3, a.getLastName());
+			ps.setString(4, a.getGender());
+			ps.setString(5, a.getBirthDate().toString());
+			ps.executeUpdate();
+			success = true;
+		} catch (SQLException e) {
+			System.out.println("Error adding actor!");
+			e.printStackTrace();
+		}
+		return success;
 	}
 
 	@Override
-	public boolean delete(Actor t) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean update(Actor a) {
+		boolean success = false;
+		String sql = "update actor set " + 
+					 " firstName = ?," + 
+					 " lastName = ?," + 
+					 " gender = ?," + 
+					 " birthdate = ?" + 
+					 "  where id = ?";
+		try (Connection conn = getConnection();
+			 PreparedStatement ps = conn.prepareStatement(sql)) {
+			ps.setString(1, a.getFirstName());
+			ps.setString(2, a.getLastName());
+			ps.setString(3, a.getGender());
+			ps.setString(4, a.getBirthDate().toString());
+			ps.setInt(5, a.getId());
+			ps.executeUpdate();
+			success = true;
+		} catch (SQLException e) {
+			System.out.println("Error updating actor!");
+			e.printStackTrace();
+		}
+		return success;
+	}
+
+	@Override
+	public boolean delete(Actor a) {
+		boolean success = false;
+		String sql = "delete from actor " + 
+				"where id = ?";
+		try (Connection conn = getConnection();
+			 PreparedStatement ps = conn.prepareStatement(sql)) {
+			ps.setInt(1, a.getId());
+			ps.executeUpdate();
+			success = true;
+		} catch (SQLException e) {
+			System.out.println("Error deleting actor!");
+			e.printStackTrace();
+		}
+		return success;
 	}
 
 }
